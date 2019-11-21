f4a2950aa863c678ef3c3e8b4e31b4963dc93242
alex0735070005 <alex0735070005@gmail.com>
2019-11-21 02:58:54 +0200

    add-task-1

---
+++ js/task1.js
@@ -0,0 +1,34 @@
+// 2 code to be will-formatted, clean and readable
+// 3 Describe in the comments why is the second console not called
+
+// Since the first condition is true, the second will not work
+let age = 12;
+
+if (age > 5) {
+  age = 5;
+  console.log('More than 5');
+} else if (age < 10) {
+  console.log('Less than 5');
+}
+
+// 4 replase if...else width the conditional (ternary) operator and switch...case
+
+let message;
+
+const login = 'Maks';
+
+switch (login) {
+  case 'Maks':
+    message = 'Hi, Maks';
+    break;
+  case 'Serg':
+    message = 'Hi, Serg';
+    break;
+  case '':
+    message = 'Hi undefined';
+    break;
+}
+
+message = message ? message : '';
+
+console.log(message);
